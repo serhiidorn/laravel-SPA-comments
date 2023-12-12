bf4d7b16c767f02975ee2508af1e977bb3cea74a
dorn <dornnsergey@gmail.com>
2023-12-12 11:27:05 +0200

    create the CommentsPage component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-
-
+import CommentsPage from './CommentsPage.vue';
 </script>
 
 <template>
     <div class="container h-100">
-
+        <CommentsPage/>
     </div>
 </template>
---
+++ resources/js/components/CommentsPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CommentForm from "./CommentForm.vue";
+</script>
+
+<template>
+    <div class="py-3 py-md-5 container">
+        <CommentForm />
+    </div>
+</template>
