edba03610092699cfdbb41079cbc884ed245701d
dorn <dornnsergey@gmail.com>
2023-12-17 13:54:21 +0200

    implement caching

--- app/Actions/GetCommentsAction.php
+++ app/Actions/GetCommentsAction.php
@@ -6,16 +6,32 @@ namespace App\Actions;
 
 use App\Models\Comment;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Cache;
 
 class GetCommentsAction
 {
     public function handle(array $requestQuery)
     {
-        return Comment::query()
-            ->whereNull('parent_id')
-            ->when(data_get($requestQuery, 'sort'), function (Builder $builder, array $column) {
-                $builder->orderBy(...$column);
-            })
-            ->paginate(25);
+        $cacheKey = $this->getCacheKey($requestQuery);
+
+        return Cache::tags('comments')->remember(
+            $cacheKey,
+            now()->addHour(),
+            function () use ($requestQuery) {
+                return Comment::query()
+                    ->whereNull('parent_id')
+                    ->when(data_get($requestQuery, 'sort'), function (Builder $builder, array $column) {
+                        $builder->orderBy(...$column);
+                    })
+                    ->paginate(25);
+            });
+    }
+
+    private function getCacheKey(array $requestQuery): string
+    {
+        $pagePart = data_get($requestQuery, 'page', 1);
+        [$sortPart, $orderPart] = data_get($requestQuery, 'sort');
+
+        return 'comments:page='.$pagePart.'&sort='.$sortPart.'&order='.$orderPart;
     }
 }
--- app/Jobs/SaveCommentJob.php
+++ app/Jobs/SaveCommentJob.php
@@ -11,6 +11,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Constraint;
 use Intervention\Image\ImageManagerStatic;
@@ -38,6 +39,8 @@ class SaveCommentJob implements ShouldQueue
         }
 
         $comment = Comment::create($this->attributes);
+
+        Cache::store('redis')->tags('comments')->flush();
 
         if ($comment instanceof Comment) {
             CommentCreated::dispatch($comment);
